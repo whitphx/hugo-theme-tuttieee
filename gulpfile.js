8f583f3d84c545730b5c3299a36e99d4f8636f55
yuichiroTCY <t.yic.yt@gmail.com>
2016-04-26 02:52:54 +0900

    Add bower and install particlejs and skeleton

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    rename = require('gulp-rename'),
+    uglify = require("gulp-uglify"),
+    filter = require('gulp-filter'),
+    gulpFilter = require('gulp-filter'),
+    bower = require('main-bower-files');
+
+gulp.task('compile:lib', function() {
+  var f = filter('**/*.js', {restore: true})
+
+  return gulp.src(bower({
+    paths: {
+      bowerJson: 'bower.json'
+    }
+  }))
+    .pipe(f)
+    .pipe(concat('lib.js'))
+    .pipe(uglify({
+      preserveComments: 'some'
+    }))
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('static/js/'))
+})
---
+++ static/js/main.js
@@ -0,0 +1,3 @@
+particlesJS.load('bg', 'assets/particlesjs-config.json', function() {
+    console.log('particles.js config loaded');
+});
