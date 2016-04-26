dee14fa9579923778bcd64c5f916eb58a2cf74ce
yuichiroTCY <t.yic.yt@gmail.com>
2016-04-26 22:39:14 +0900

    Modify urls as absolute paths

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 (function() {
   if (document.getElementById('bg')) {
-    particlesJS.load('bg', '/assets/particlesjs-config.json', function() {
+    particlesJS.load('bg', rootURI + '/assets/particlesjs-config.json', function() {
         console.log('particles.js config loaded');
     });
   }
